d3bed1aa5d4f3966798a0a420b0aa634965cb672
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-09-26 22:50:18 +0900

    chore(comment2ndjson): add rollup settings

--- packages/comment2ndjson-lib/rollup.config.ts
+++ packages/comment2ndjson-lib/rollup.config.ts
@@ -1,3 +1,12 @@
-export default [
-  {}
-]
+import { defineConfig } from 'rollup';
+
+export default defineConfig([
+  {
+    input: "src/comment2ndjson-lib.ts",
+    output: {
+      dir: "dist/es6",
+      sourcemap: true,
+      format: "es",
+    }
+  }
+]);
