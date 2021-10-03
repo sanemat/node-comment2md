fb67e1ea79b2f4768f8615409bd1f7704c0fd4ee
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-10-03 13:00:21 +0900

    chore(example): add

--- packages/comment2ndjson-lib/example.ts
+++ packages/comment2ndjson-lib/example.ts
@@ -1,10 +1,10 @@
 import { comment2ndjsonLib } from "src/comment2ndjson-lib";
 
 const ts = `
-function foo() {
-  /** @e2e example */
-  return 1;
-}
+console.log(foo)
+/** @e2e message1 */
+/** @example message2 */
+/** @e2e message2 */
 `;
 
-console.log(await comment2ndjsonLib("test/foo.ts", ts)); // {"test/foo.ts": [{"e2e": "message1"}, {"e2e": "message2"}]}
+console.log(await comment2ndjsonLib("test/foo.ts", ts, "e2e")); // {"test/foo.ts": [{"e2e": "message1"}, {"e2e": "message2"}]}
