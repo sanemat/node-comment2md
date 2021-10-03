36cc5c1c9bad5dbf72730390ff4333fac5d721f6
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-10-03 22:30:20 +0900

    chore(comment2ndjson): without filter

--- packages/comment2ndjson-lib/src/comment2ndjson-lib.test.ts
+++ packages/comment2ndjson-lib/src/comment2ndjson-lib.test.ts
@@ -1,15 +1,14 @@
 import { strict as assert } from "assert";
 import { comment2ndjsonLib } from "./comment2ndjson-lib";
 
-const ts = `
+(async () => {
+  const ts = `
 console.log(foo)
 /** @e2e message1 */
-/** @example message2 */
 /** @e2e message2 */
 `;
 
-(async () => {
-  assert.deepEqual(await comment2ndjsonLib("test/foo.ts", ts, ["e2e"]), {
+  assert.deepEqual(await comment2ndjsonLib("test/foo.ts", ts), {
     path: "test/foo.ts",
     comments: [
       { tag: "e2e", message: "message1" },
