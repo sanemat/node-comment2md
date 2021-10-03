ab7f7614b45ab0369f94a5e580f590bc226b3976
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-10-03 14:24:45 +0900

    chore(comment2ndjson): fake implement

--- packages/comment2ndjson-lib/src/comment2ndjson-lib.ts
+++ packages/comment2ndjson-lib/src/comment2ndjson-lib.ts
@@ -1,4 +1,8 @@
-function comment2ndjsonLib(): number {
-  return 1;
+async function comment2ndjsonLib(
+  path: string,
+  content: string,
+  filter: [string]
+): Promise<string> {
+  return `{"test/foo.ts": [{"e2e": "message1"}, {"e2e": "message2"}]}`;
 }
 export { comment2ndjsonLib };
